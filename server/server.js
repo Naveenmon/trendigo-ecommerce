b9f06e9cd9bb6737ee2d16579dec981f159a2b5c
Naveen <naveenanand173@gmail.com>
2024-11-16 18:37:29 +0530

    fix for access control

--- server/server.js
+++ server/server.js
@@ -58,7 +58,16 @@ app.use(
   })
 );
 
-app.options("*", cors())
+app.options("*", (req, res) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Cache-Control, Expires, Pragma"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.sendStatus(204);
+});
 
 app.use(cookieParser());
 app.use(express.json());
